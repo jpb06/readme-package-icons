06b161388993e34ab0625744c6d35654ac3a051e
jpb06 <jp.bois.06@outlook.fr>
2022-07-11 22:40:23 +0200

    codesmell

--- src/logic/package/get-dependencies.ts
+++ src/logic/package/get-dependencies.ts
@@ -3,10 +3,8 @@ import { readJson } from 'fs-extra';
 export const getDependencies = async (path: string): Promise<Array<string>> => {
   const json = await readJson(`${path}/package.json`);
 
-  const dependencies = [
+  return [
     ...Object.keys(json.dependencies),
     ...Object.keys(json.devDependencies),
   ];
-
-  return dependencies;
 };
