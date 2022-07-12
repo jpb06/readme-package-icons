c01983905e2f2bfe311c897e1c8e25642f730056
jpb06 <jp.bois.06@outlook.fr>
2022-07-13 01:00:19 +0200

    fixing test

--- src/logic/icons-list/build-icons-list.test.ts
+++ src/logic/icons-list/build-icons-list.test.ts
@@ -6,16 +6,19 @@ describe('buildIconsList function', () => {
   it('should generate images without links', () => {
     const result = buildIconsList(technosWithoutLinks, 60);
 
-    const regex = /<img height="60" src="\S+" \/>&nbsp;/g;
-    const count = (result.match(regex) || []).length;
+    const imgRegex = /<img height="60" src="\S+" \/>/g;
+    const imgCount = (result.match(imgRegex) || []).length;
+    expect(imgCount).toBe(3);
 
-    expect(count).toBe(3);
+    const spaceRegex = /&nbsp;/g;
+    const spacesCount = (result.match(spaceRegex) || []).length;
+    expect(spacesCount).toBe(2);
   });
 
   it('should generate links around images', () => {
     const result = buildIconsList(technosWithLinks, 100);
 
-    const regex = /<a href="\S+"><img height="100" src="\S+" \/><\/a>&nbsp;/g;
+    const regex = /<a href="\S+"><img height="100" src="\S+" \/><\/a>/g;
     const count = (result.match(regex) || []).length;
 
     expect(count).toBe(3);
